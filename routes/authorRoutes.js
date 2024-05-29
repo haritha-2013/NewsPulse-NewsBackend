675c55e77492fb1d17e276adf7b722837f7e0d2e
Haritha <haritha.abhilash2013@gmail.com>
2024-05-29 16:04:13 +0530

    Made changes in authorRoutes.js file

--- routes/authorRoutes.js
+++ routes/authorRoutes.js
@@ -6,16 +6,16 @@ const router = express.Router()
 router.get('/', getAllAuthors )
 
 //2. Get author byId
-router.get('/:authorById', getAuthorById)
+router.get('/:authorId', getAuthorById)
 
 //3. Add new author
 router.post('/', addNewAuthor)
 
 //4. Update an author
-router.patch('/:authorById', updateAuthor)
+router.patch('/:authorId', updateAuthor)
 
 //5. Delete an author
-router.delete('/:authorById', deleteAuthor)
+router.delete('/:authorId', deleteAuthor)
 
 
 module.exports = router
